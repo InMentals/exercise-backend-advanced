4bccef3973c18fcc534304325d5a24b03a095947
Pabo Cervigon <pablocervigon@gmail.com>
2025-06-29 23:48:57 +0200

    initDB upgraded

--- initDB.js
+++ initDB.js
@@ -49,22 +49,18 @@ async function initProducts() {
       name: "Volkswagen Golf",
       owner: admin._id,
       price: 20000,
-      image:
-        "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2024/01/volkswagen-golf-3265600.jpg?tf=1200x",
       tags: ["motor", "lifestyle"],
     },
     {
       name: "Iphone 13",
       owner: admin._id,
       price: 900,
-      image: "https://i.blogs.es/5afa41/iphone-13-mini-12-/650_1200.jpg",
       tags: ["mobile", "work"],
     },
     {
       name: "Bosch drill machine",
       owner: user._id,
       price: 150,
-      image: "https://m.media-amazon.com/images/I/61T3Nj83OuL.jpg",
       tags: ["work"],
     },
   ]);
